c7c1a39c210d26805fa27643eba7bebc789a9f6c
Splines <dominic-plein@gmx.de>
2022-04-05 21:14:59 +0200

    Add concrete implementations of ValueCard (+UnitTests)
    This includes MoneyCard, PointCard and CurseCard which are all
    extending ValueCard.

---
+++ 3-dominion-domain/src/main/java/me/splines/dominion/Card/MoneyCard.java
@@ -0,0 +1,17 @@
+package me.splines.dominion.Card;
+
+public class MoneyCard extends ValueCard {
+
+    public MoneyCard(String name, int cost, int money) {
+        super(name, CardType.MONEY, cost, money);
+
+        if (money < 0) {
+            throw new IllegalArgumentException("'Money' must be a positive value");
+        }
+    }
+
+    public int getMoney() {
+        return getValue();
+    }
+
+}
