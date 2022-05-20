e3be30ba2118bbeda59c711015811d31cf4b4a06
Splines <dominic-plein@gmx.de>
2022-05-20 15:02:29 +0200

    Init Jacoco & Codecov
    (included in GitHub workflow)

---
+++ dominion-report/src/test/java/me/splines/dominion/ReportDummyTest.java
@@ -0,0 +1,13 @@
+package me.splines.dominion;
+
+import org.junit.jupiter.api.Test;
+
+class ReportDummyTest {
+
+    @Test
+    void dummyReportTest() {
+        // see information for why we need this file
+        // in this module's pom.xml
+    }
+
+}
