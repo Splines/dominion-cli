e492ae0c9cf3c93b1ce161a7f3ff8fdd38a6bd38
Splines <dominic-plein@gmx.de>
2022-05-24 12:54:36 +0200

    Print start screen
    also added CI passing badge to readme

--- 0-dominion-plugin-database/src/main/java/me/splines/dominion/Dominion.java
+++ 0-dominion-plugin-database/src/main/java/me/splines/dominion/Dominion.java
@@ -7,10 +7,26 @@ import me.splines.dominion.game.Game;
 public class Dominion {
 
     public static void main(String[] args) {
+        printStartScreen();
+
         GameCLI cli = new GameCLI();
         List<String> names = cli.getPlayerNames();
         Game game = new Game(cli, names);
         game.start();
+    }
+
+    private static void printStartScreen() {
+        System.out.println("🃏🃏🃏🃏🃏🃏🃏🃏🃏🃏🃏");
+        System.out.println("🃏🃏 Dominion CLI 🃏🃏");
+        System.out.println("🃏🃏  by Splines  🃏🃏");
+        System.out.println("🃏🃏🃏🃏🃏🃏🃏🃏🃏🃏🃏");
+        System.out.println();
+        System.out.println("Welcome to Dominion - a deck-building card game.");
+        System.out.println("Have fun with this CLI, e.g. use it to train");
+        System.out.println("an artificial intelligence.");
+        System.out.println("If there are any bugs, please report them on GitHub:");
+        System.out.println("https://github.com/splines/dominion-cli");
+        System.out.println();
     }
 
 }
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # dominion-cli
 Play the dominion card game via a CLI
 
+[![Java CI with Maven](https://github.com/Splines/dominion-cli/actions/workflows/build.yml/badge.svg)](https://github.com/Splines/dominion-cli/actions/workflows/build.yml)
 [![codecov](https://codecov.io/gh/Splines/dominion-cli/branch/main/graph/badge.svg?token=kG7xahROjx)](https://codecov.io/gh/Splines/dominion-cli)
