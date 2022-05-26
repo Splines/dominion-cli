ce4c2d951b6da77f3c84f339e2d301a7b40929b0
Splines <dominic-plein@gmx.de>
2022-05-26 21:34:07 +0200

    Change point card symbol

--- 0-dominion-plugin-cli/src/main/java/me/splines/dominion/card/body/PointCardFormatter.java
+++ 0-dominion-plugin-cli/src/main/java/me/splines/dominion/card/body/PointCardFormatter.java
@@ -9,10 +9,10 @@ public class PointCardFormatter extends CardBodyFormatter<PointCard> {
     public String getBody(PointCard card) {
         StringBuilder body = new StringBuilder();
         body.append(getEmptyLineWithBorder());
-        body.append(getTextToTheRight("📀📀📀"));
+        body.append(getTextToTheRight("🟩🟩🟩"));
         // Assumption: card money is just one digit
-        body.append(getTextToTheRight("📀" + card.getPoints() + " 📀"));
-        body.append(getTextToTheRight("📀📀📀"));
+        body.append(getTextToTheRight("🟩" + card.getPoints() + " 🟩"));
+        body.append(getTextToTheRight("🟩🟩🟩"));
         body.append(getEmptyLinesWithBorder(CardFormatter.CARD_BODY_HEIGHT - 3));
         return body.toString();
     }
