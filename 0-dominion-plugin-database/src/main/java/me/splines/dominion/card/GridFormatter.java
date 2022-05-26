3031682c6e2c326b21386ef99404bc302c5c9175
Splines <dominic-plein@gmx.de>
2022-05-26 20:54:20 +0200

    Simplify GridFormatter by method extraction

--- 0-dominion-plugin-database/src/main/java/me/splines/dominion/card/GridFormatter.java
+++ 0-dominion-plugin-database/src/main/java/me/splines/dominion/card/GridFormatter.java
@@ -1,5 +1,7 @@
 package me.splines.dominion.card;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import me.splines.dominion.card.FormatExceptions.CardsDifferentHeightsException;
@@ -21,39 +23,11 @@ public class GridFormatter {
     private static String constructFormattedGrid(List<Card> cards, boolean withIndex) {
         StringBuilder grid = new StringBuilder();
 
-        int i = 0;
-        int printIndex;
         // Go through all rows
+        int i = 0;
         while (i < cards.size()) {
-            Card leftmostCard = cards.get(i);
-            printIndex = withIndex ? i + 1 : -1;
-            String[] linesLeftmostCard = CardFormatter
-                    .getFormattedWithIndex(leftmostCard, printIndex)
-                    .split("\n");
-            String[] cardRowLines = linesLeftmostCard.clone();
-
-            // Cards to the right in same row
-            for (int j = 0; j < GRID_WIDTH - 1; j++) {
-                i++;
-                if (i >= cards.size())
-                    break;
-
-                Card cardToTheRight = cards.get(i);
-                printIndex = withIndex ? i + 1 : -1;
-                String[] linesCardToTheRight = CardFormatter
-                        .getFormattedWithIndex(cardToTheRight, printIndex)
-                        .split("\n");
-                if (linesCardToTheRight.length != linesLeftmostCard.length)
-                    throw new CardsDifferentHeightsException(List.of(leftmostCard, cardToTheRight));
-
-                // Combine lines of one card row
-                for (int k = 0; k < cardRowLines.length; k++) {
-                    cardRowLines[k] += " ".repeat(GRID_HGAP);
-                    cardRowLines[k] += linesCardToTheRight[k];
-                }
-            }
-
-            i++;
+            List<String> cardRowLines = getCardStringForOneRow(i, cards, withIndex);
+            i += GRID_WIDTH; // i is now at the first card of next row
 
             // Combine all lines of one card row
             String row = String.join("\n", cardRowLines);
@@ -63,6 +37,53 @@ public class GridFormatter {
         }
 
         return grid.toString();
+    }
+
+    /**
+     * Returns the card in the same row as string array.
+     *
+     * @param i
+     * @param cardsSize
+     * @return
+     */
+    private static List<String> getCardStringForOneRow(int i, List<Card> cards, boolean withIndex) {
+        // Leftmost card
+        Card leftmostCard = cards.get(i);
+        String[] linesLeftmostCard = CardFormatter
+                .getFormattedWithIndex(leftmostCard, withIndex ? i + 1 : -1)
+                .split("\n");
+        List<String> cardRowLines = new ArrayList<>(Arrays.asList(linesLeftmostCard));
+
+        // Cards to the right in same row
+        for (int j = 0; j < GRID_WIDTH - 1; j++) {
+            i++;
+            if (i >= cards.size())
+                break;
+
+            Card cardToTheRight = cards.get(i);
+            String[] linesCardToTheRight = CardFormatter
+                    .getFormattedWithIndex(cardToTheRight, withIndex ? i + 1 : -1)
+                    .split("\n");
+            if (linesCardToTheRight.length != linesLeftmostCard.length)
+                throw new CardsDifferentHeightsException(List.of(leftmostCard, cardToTheRight));
+
+            combineLinesOfOneCardRow(cardRowLines, linesCardToTheRight);
+        }
+
+        return cardRowLines;
+    }
+
+    /**
+     * Adds rest of cards in the row to the lines (inline!).
+     *
+     * @param cardRowLines
+     * @param newLines
+     */
+    private static void combineLinesOfOneCardRow(List<String> cardRowLines, String[] newLines) {
+        for (int k = 0; k < cardRowLines.size(); k++) {
+            String append = " ".repeat(GRID_HGAP) + newLines[k];
+            cardRowLines.set(k, cardRowLines.get(k) + append);
+        }
     }
 
 }
